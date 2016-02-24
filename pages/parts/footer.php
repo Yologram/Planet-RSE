981f7dc8e858805add0c83225972aa72d54da614
seb <sebast33500@gmail.com>
2016-02-24 17:24:02 +0100

    incons reseau sociaux

--- pages/parts/footer.php
+++ pages/parts/footer.php
@@ -1,8 +1,12 @@
 
 
 <div class="footer">
+	<i class="twitter icon"></i>
+	<i class="facebook icon"></i>
+	<i class="google plus icon"></i>
+	<i class="linkedin icon"></i>
 		<p><a href="">Mentions légales</a></p>
 	</div>
-	
+
 </body>
 </html>
