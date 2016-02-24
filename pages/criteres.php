92f573d5ca6193c20944fe02ab9d74fe0c860322
Yologram <metais.louis@hotmail.fr>
2016-02-24 14:41:54 +0100

    ajout de la page criteres

--- pages/criteres.php
+++ pages/criteres.php
@@ -1,6 +1,83 @@
 <?php
 include ('parts/header.php');
 ?>
+<div class="ui stackable container">
+	<div class="ui stackable grid">
+		<h1 class="ui dividing header">Elaboration des critères</h1>
+		<p>Une première phase d’échanges a permis de déterminer les attentes respectives des acheteurs et des entreprises. Une deuxième étape a consisté à passer en revue l’ensemble des labels et normes actuels ayant trait à la RSE : Global Compact, SA 8000, Nouvelles Régulations Economiques (Loi NRE), Global Reporting Initiative (GRI), Bilan Social, ISO 26000, label LUCIE, VIGEO… Ces phases préalables ont servi de point de départ à la création d’une première version d’indicateurs combinant les attentes des parties prenantes et s’inspirant des référentiels existants. Ces critères ont été testés auprès d’acteurs privés : entreprises membres du CJD, entreprises membres de la Fédération Régionale des Travaux Publics (FRTP), entreprises du secteur de l’imprimerie… Les retours du terrain ont permis d’affiner les indicateurs et de créer une nouvelle version, elle-même à nouveau testée sur le terrain, etc.</p>
+		<h1 class="ui dividing header">Critères universels ?</h1>
+		<p>Faut il sectoriser la critérisation ou faut-il au contraire créer des indicateurs universels qui soient applicables à toutes les entreprises et organisations ?
+			Trois possibilités d’évaluation ont été abordées :</p>
+			<ol>
+				<li>Critères universels</li>
+			</br>
+			<p>Chaque critère doit pouvoir s’appliquer à toute forme d’entreprise. Une unique grille d’évaluation permet d’évaluer tous types d’entreprises. Le nombre d’indicateurs est réduit car le référentiel universel ne peut aborder les spécificités de chaque secteur d’activité. Il offre cependant l’avantage de présenter un outil à la fois simple et lisible par l’ensemble des acteurs du marché.</p>
+			<li>Critères par secteur d’activité</li>
+		</br>
+		<p>Une grille de critères propre à un secteur donné ou à une branche d’activité (ex : nettoyage, bâtiment, métallurgie) permet de mieux calibrer les indicateurs en fonction des spécificités d’un secteur. Il est difficile en revanche de mener une comparaison entre deux secteurs d’activité puisque les indicateurs retenus diffèrent entre l’un et l’autre.</p>
+		<li>Mix des deux formats précédents</li>
+	</br>
+	<p>En plus d’un socle commun d’indicateurs incontournables (handicap, insertion…), des indicateurs élaborés en fonction des spécificités de chaque secteur d’activité viennent se greffer. Cette méthode permet de rendre possible la comparaison entre des entreprises aux activités très différentes tout en offrant un outil précis d’analyse au sein d’un secteur d’activité donné.</p>
+</ol>
+</div>
+<div class="ui stackable four column grid">
+	<h1 class=" ui dividing header">Cinq volets</h1>
+	<p>Les critères ont été répartis en cinq groupes : « économique », « social » et « environnemental », « territorial » et « gouvernance ». Ces groupes recouvrent eux-mêmes plusieurs sous-catégories :</p>
+	<div class="four wide column">
+		<div class="ui inverted segment">
+			<div class="ui inverted relaxed divided list">
+				<h3 class="header">Critères environnementaux</h3>
+				<div class="item">Prévention de la pollution</div>
+				<div class="item">Utilisation durable des ressources</div>
+				<div class="item">Eco-conception</div>
+				<div class="item">Management environnemental</div>
+			</div>
+		</div>
+	</div>
+	<div class="four wide column">
+		<div class="ui inverted segment">
+			<div class="ui inverted relaxed divided list">
+				<h3 class="header">Critères de gouvernance</h3>
+				<div class="item">Conditions de travail</div>
+				<div class="item">Droits fondamentaux</div>
+				<div class="item">Formalisation d’une politique RSE</div>
+			</div>
+		</div>
+	</div>
+	<div class="four wide column">
+		<div class="ui inverted segment">
+			<div class="ui inverted relaxed divided list">
+				<h3 class="header">Critères sociétaux</h3>
+				<div class="item">Publication du bilan et du compte de résultat</div>
+				<div class="item">Ancrage territorial</div>
+			</div>
+		</div>
+	</div>
+	<div class="four wide column">
+		<div class="ui inverted segment">
+			<div class="ui inverted relaxed divided list">
+				<h3 class="header">Critères sociaux :</h3>
+				<div class="item">Droits fondamentaux</div>
+				<div class="item">Discrimination/Diversité</div>
+				<div class="item">Santé et sécurité</div>
+				<div class="item">Conditions de travail</div>
+				<div class="item">Emploi</div>
+			</div>
+		</div>
+	</div>
+	<div class="four wide column">
+		<div class="ui inverted segment">
+			<div class="ui inverted relaxed divided list">
+				<h3 class="header">Critères économiques :</h3>
+				<div class="item">Structure d’entreprise adaptée aux besoins du marché</div>
+				<div class="item">Santé financière</div>
+				<div class="item">Pérennité de l’entreprise</div>
+				<div class="item">Respect des fournisseurs</div>
+				<div class="item">Respect des publications du Bilan et du Compte de Résultat au greffe du tribunal de commerce</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 
