21c525c6bb7665b3b2b5d95e60a3bd1e5fd741f8
Robin <robin.sangla@live.fr>
2016-02-24 20:25:01 +0100

    boutons

--- pages/parts/footer.php
+++ pages/parts/footer.php
@@ -1,18 +1,18 @@
 
 <div class="footer">
 <div class="boutons_footer">
-	<button class="ui circular facebook icon button">
+	<a href="https://www.facebook.com/Cours-rousselot-voltaire-399708106721229/?fref=ts" target="_blank"><button class="ui circular facebook icon button">
 		<i class="facebook icon"></i>
-	</button>
-	<button class="ui circular google plus icon button">
+	</button></a>
+	<a href="http://www.zombo.com" target="_blank"><button class="ui circular google plus icon button">
 		<i class="google plus icon"></i>
-	</button>
-	<button class="ui circular linkedin icon button">
+	</button></a>
+	<a href="https://www.facebook.com/AmbreAkasora?fref=ts" target="_blank"><button class="ui circular linkedin icon button">
 		<i class="linkedin icon"></i>
-	</button>
-	<button class="ui circular youtube icon button">
+	</button></a>
+	<a href=""><button class="ui circular youtube icon button" target="_blank">
 		<i class="youtube icon"></i>
-	</button> 
+	</button> </a>
 </div>
 	<p><a href="">Mentions légales</a></p>
 </div>
