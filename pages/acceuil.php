c0c1d50a6fa15f86dce4d2b6e157007f9b754a4a
Robin <robin.sangla@live.fr>
2016-02-24 16:40:23 +0100

    Page Accueil

--- pages/acceuil.php
+++ pages/acceuil.php
@@ -2,9 +2,48 @@
 include ('parts/header.php');
 ?>
 
+<div class="ui center aligned grid">
+<div class="ui nine wide left aligned column">
+<h1 class="ui dividing header">Bienvenue</h1>
+
+<p>Bienvenue sur le site de Planet’RSE, Plateforme de Notation et d’Evaluation Territoriale de la RSE.
+L’association a pour vocation de promouvoir la Responsabilité Sociétale des Entreprises et des organisations en élaborant des critères de notation en matière de RSE utilisés pour évaluer chacun de ses membres.</p>
+</br>
+</br>
+
+<img class="ui massive image" src="../images/accueil.jpg">
+
+<h1 class="ui dividing header">Nos dernières actualités</h1>
+</br>
+  <div class="ui segment">
+    <div class="event">
+      <div class="content">
+        <h3 class="summary">
+          14 décembre 2015
+        </h3>
+        <div class="extra text">
+          Cap sur 2016 ! Forte d’une année riche en audits et en déploiement, l’association Planet’RSE organise une réunion de clôture de l’année  et une présentation des objectifs 2016. S’en suivra un temps d’échanges avec tous les invités autour d’un cocktail convivial. L’événement aura lieu à Nantes le 14 décembre de 18h30 à 20h30. Inscription libre et gratuite via le lien suivant : INSCRIPTION
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="ui segment">
+    <div class="event">
+      <div class="content">
+        <h3 class="summary">
+          27 novembre 2015
+        </h3>
+        <div class="extra text">
+          Planet’RSE passe à la radio ! Vous pouvez retrouver l’interview complète de Gilles Rampillon via le lien suivant : 7-9h Sun Radio
+        </div>
+      </div>
+    </div>
+  </div>
 
 
-
+</div>
+</div>
 <?php
 include ('parts/footer.php');
 ?>
